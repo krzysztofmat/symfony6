01100c454f84f3614356740428809b518cf5e617
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-04 14:05:23 +0200

    after ch04 - wildcard routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,6 +2,8 @@
  
 namespace App\Controller;
 
+use function Symfony\Component\String\u;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,5 +13,17 @@ class VinylController
     public function homepage(): Response
     {
         return new Response('Title: "PB & Jams"');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(?string $slug = null): Response
+    {
+        if ($slug) {
+            $title = u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = "All genres";
+        }
+
+        return new Response('Genre: ' . $title);
     }
 }
