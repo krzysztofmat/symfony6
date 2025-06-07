3e25deef5d65516853c5da7df30fa26787475cac
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-07 12:41:58 +0200

    after ch14 - service objects

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/song/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
+        $logger->info('Returning API response for song {song}.', ['song' => $id]);
         // TODO query the database
         $song = [
             'id' => $id,
