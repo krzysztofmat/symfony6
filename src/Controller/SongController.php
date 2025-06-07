0dd72ed8719f9e395850e6297cc76e7ce472018c
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-07 12:23:39 +0200

    after ch13 - smart routes get only and validate wildcards

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -9,8 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/song/{id}')]
-    public function getSong($id): Response
+    #[Route('/api/song/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id): Response
     {
         // TODO query the database
         $song = [
